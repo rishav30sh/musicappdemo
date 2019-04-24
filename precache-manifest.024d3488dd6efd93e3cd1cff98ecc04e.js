94a43805689dc69d7bbdab9262e59377cff0c9cd
Rishav <rishav30sharma@gmail.com>
2019-04-24 13:45:06 +0530

    Updates

---
+++ precache-manifest.024d3488dd6efd93e3cd1cff98ecc04e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "80ad3f8ae40d746e686e",
+    "url": "/static/js/main.049dbf3e.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "80ad3f8ae40d746e686e",
+    "url": "/static/css/main.781b43f8.chunk.css"
+  },
+  {
+    "revision": "6d9e3e3459f3a4aa3aeb522450051530",
+    "url": "/index.html"
+  }
+];
